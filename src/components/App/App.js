41a1437934d39ccab552d316fd6dd624472e66c6
Vladislav Pavlov <vladislawpavlov@gmail.com>
2023-01-31 16:59:06 -0500

    сверстана страница PageNotFound

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,6 +6,7 @@ import SavedMovies from '../SavedMovies/SavedMovies';
 import Profile from '../Profile/Profile';
 import Login from '../Login/Login';
 import Register from '../Register/Register';
+import PageNotFound from '../PageNotFound/PageNotFound'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/signin" element={<Login />} />
         <Route path="/signup" element={<Register />} />
+        <Route path='*' element={<PageNotFound />} />
       </Routes>
     </div>
   );
