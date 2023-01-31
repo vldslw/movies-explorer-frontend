a09b9381d903061fea8d3433558b530bb0943d51
Vladislav Pavlov <vladislawpavlov@gmail.com>
2023-01-31 17:15:58 -0500

    подключены шрифты

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -6,8 +6,8 @@ function MoviesCard(props) {
   return (
     <article className="card">
       <div className='card__desc'>
-        <h2 className='card__name'>'В погоне за Бенкси'</h2>
-        <p className='card__duration'>'27 минут'</p>
+        <h2 className='card__name'>В погоне за Бенкси</h2>
+        <p className='card__duration'>27 минут</p>
       </div>
       <img className="card__image" src={moviePic} alt="Картинка" />
       <button className={`card__button ${props.buttonClassName}`}>Сохранить</button>
