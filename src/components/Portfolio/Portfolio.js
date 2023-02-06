48f2b9af6bf55079b1d05098cf3e9fc2cc95c084
Vladislav Pavlov <vladislawpavlov@gmail.com>
2023-02-06 01:20:04 -0500

    ссылки в портфолио реализованы через список

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -5,20 +5,26 @@ function Portfolio() {
   return (
     <div className="portfolio">
       <h2 className="portfolio__title">Портфолио</h2>
-      <div className="portfolio__links">
-        <a href="https://github.com/vldslw/how-to-learn" className="portfolio__link" target="_blank" rel="noreferrer">
-          <p className='portfolio__link-text'>Статичный сайт</p>
-          <img className="portfolio__arrow" src={arrow} alt="Стрелка" />
-        </a>
-        <a href="https://github.com/vldslw/russian-travel" className="portfolio__link" target="_blank" rel="noreferrer">
-          <p className='portfolio__link-text'>Адаптивный сайт</p>
-          <img className="portfolio__arrow" src={arrow} alt="Стрелка" />
-        </a>
-        <a href="https://github.com/vldslw/react-mesto-api-full" className="portfolio__link" target="_blank" rel="noreferrer">
-          <p className='portfolio__link-text'>Одностраничное приложение</p>
-          <img className="portfolio__arrow" src={arrow} alt="Стрелка" />
-        </a>
-      </div>
+      <ul className="portfolio__links">
+        <li className='portfolio__item'>
+          <a href="https://github.com/vldslw/how-to-learn" className="portfolio__link" target="_blank" rel="noreferrer">
+            <p className='portfolio__link-text'>Статичный сайт</p>
+            <img className="portfolio__arrow" src={arrow} alt="Стрелка" />
+          </a>
+        </li>
+        <li className='portfolio__item'>
+          <a href="https://github.com/vldslw/russian-travel" className="portfolio__link" target="_blank" rel="noreferrer">
+            <p className='portfolio__link-text'>Адаптивный сайт</p>
+            <img className="portfolio__arrow" src={arrow} alt="Стрелка" />
+          </a>  
+        </li>
+        <li className='portfolio__item'>
+          <a href="https://github.com/vldslw/react-mesto-api-full" className="portfolio__link" target="_blank" rel="noreferrer">
+            <p className='portfolio__link-text'>Одностраничное приложение</p>
+            <img className="portfolio__arrow" src={arrow} alt="Стрелка" />
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
