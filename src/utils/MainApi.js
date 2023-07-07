b36b8de90511e1dd8e6045c07a05f4f82396ffdc
Vladislav Pavlov <vladislawpavlov@gmail.com>
2023-07-06 22:49:25 -0300

    изменен url бэкенда

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -118,7 +118,7 @@ class MainApi {
 
 const mainApi = new MainApi({
   // baseUrl: 'https://api.beatfilm-vldslw.nomoredomains.club',
-  baseUrl: 'https://movies-explorer-api-u78y.onrender.com',
+  baseUrl: 'https://movies-explorer-api-ni81.onrender.com',
 });
 
 export default mainApi;
